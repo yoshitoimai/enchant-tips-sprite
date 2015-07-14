b842da290ad8c65ffda9a1e5091b9c1943327e28
Yoshito Imai <yoshito.imai@gmail.com>
2015-07-14 18:56:20 +0900

    create project

---
+++ custom.js
@@ -0,0 +1,3 @@
+gameStart = function(core) {
+
+};
---
+++ main.js
@@ -0,0 +1,22 @@
+enchant();
+
+var core;
+
+// ゲーム画面の幅と高さ
+var DISPLAY_WIDTH  = 320;
+var DISPLAY_HEIGHT = 480;
+var FPS = 16;
+var RECTANGLE_SIZE = 16;
+
+window.onload = function() {
+	core = new Core(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	core.fps = FPS;
+
+	// 画像の読み込みが完了した時に呼ばれます
+	core.onload = function(){
+		gameStart(core);
+	};
+
+	// ゲームを開始します
+	core.start();
+}
