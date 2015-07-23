dc581962a2216be70598324c25bffda4bb02cf3c
Yoshito Imai <yoshito.imai@gmail.com>
2015-07-23 23:33:20 +0900

    draw image

--- custom.js
+++ custom.js
@@ -1,7 +1,13 @@
 preroadImage = function(core) {
 
+	core.preload('sample.png');
+
 };
 
 gameStart = function(core) {
+
+	var sprite = new ActionSprite('sample.png', 32, 32);
+
+	core.rootScene.addChild(sprite);
 
 };
