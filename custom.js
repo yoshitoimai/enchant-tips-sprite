21020e91f06e8835b80ce60b856f91db8bad091d
Yoshito Imai <yoshito.imai@gmail.com>
2015-07-24 02:26:44 +0900

    change sprite.x

--- custom.js
+++ custom.js
@@ -10,7 +10,7 @@ gameStart = function(core) {
 
 		var sprite = new ActionSprite('asteroid_1.png', 32, 32);
 
-		sprite.x = 144;
+		sprite.x = getRandom(100, 200);
 		sprite.y = 0;
 
 		sprite.tl.moveBy(0, 224, 50).then(function() {
